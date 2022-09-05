99f1f7c54554d473d4fe18320018b4e3dfd3f28c
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-05 05:13:53 -0400

    Add basic test

--- src/generator.ts
+++ src/generator.ts
@@ -2,7 +2,10 @@ import * as core from "@actions/core";
 import { Inputs } from "./inputs";
 import { execSync } from "child_process";
 
-export async function generateDotEnvFile({ templatePath, outputPath }: Inputs): Promise<void> {
+export async function generateDotEnvFile({
+  templatePath,
+  outputPath,
+}: Inputs): Promise<void> {
   core.info("Generating dotenv file ...");
   execSync(`envsubst < ${templatePath} > ${outputPath}`, { env: process.env });
 }
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -8,13 +8,9 @@ const DOTENV_TEMPLATE_LINES = [
   "VAR_C=${VAR_C}",
   "VAR_B=${VAR_B}",
   "VAR_A=${VAR_A}",
-]
+];
 
-const DOTENV_LINES = [
-  "VAR_C=c",
-  "VAR_B=b",
-  "VAR_A=a",
-]
+const DOTENV_LINES = ["VAR_C=c", "VAR_B=b", "VAR_A=a"];
 
 const TEMPLATE_PATH = path.join(__dirname, ".env.template.tmp");
 const OUTPUT_PATH = path.join(__dirname, ".env.tmp");
@@ -25,9 +21,14 @@ test("test if output value from action is same file as expected", async () => {
   process.env.VAR_C = "c";
   const templateFileContent = DOTENV_TEMPLATE_LINES.join("\n");
   fs.writeFileSync(TEMPLATE_PATH, templateFileContent);
-  await generateDotEnvFile({ templatePath: TEMPLATE_PATH, outputPath: OUTPUT_PATH });
-  fs.readFileSync(OUTPUT_PATH, "utf8").split("\n").forEach((line, index) => {
-    expect(line).toEqual(DOTENV_LINES[index]);
+  await generateDotEnvFile({
+    templatePath: TEMPLATE_PATH,
+    outputPath: OUTPUT_PATH,
   });
+  fs.readFileSync(OUTPUT_PATH, "utf8")
+    .split("\n")
+    .forEach((line, index) => {
+      expect(line).toEqual(DOTENV_LINES[index]);
+    });
   execSync(`find ${__dirname} -name "*.tmp" -type f -delete`);
 });
