5f942d123bf3e90713d1181dd423c6349fb8fae6
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2022-09-05 20:38:18 -0400

    Allow hardcoded vars

--- src/env.ts
+++ src/env.ts
@@ -13,8 +13,9 @@ export async function prepareEnv({
   const envObject = dotenv.parse(templateFileContents);
   const requiredVarKeys = Object.keys(envObject);
   const missingKeys: string[] = [];
-  requiredVarKeys.forEach((key) => {
-    if (!process.env[key]) {
+  Object.entries(envObject).forEach(([key, value]) => {
+    const valueIsUndefined = value === undefined || (typeof value === "string" && value.startsWith("$"));
+    if (valueIsUndefined && !process.env[key]) {
       core.warning(`Environment variable ${key} is not set`);
       missingKeys.push(key);
     }
